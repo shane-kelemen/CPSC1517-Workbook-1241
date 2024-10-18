6c94b43fc61f839d838c72d4769f757d004dbb78
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2024-10-17 22:27:26 -0600

    Completed Controls Components and Began Data Entry October 17

---
+++ MyFirstBlazorSolution/MyFirstBlazorApp/Components/Pages/ExamplesPages/DataEntry.razor.cs
@@ -0,0 +1,32 @@
+﻿namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
+{
+    // This must be a partial class to properly interact with the .razor page
+    // The two together are considered to be the entire class definition
+    public partial class DataEntry
+    {
+        private string feedback; 
+
+        // Dictionaries are associative containers, which means a "value" can be accessed by
+        // specifying the "Key".  Both the Key and the Value may be any data type.
+        private Dictionary<string, string> errorMessages = new Dictionary<string, string>();
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            errorMessages.Add("Test", "My more detailed message");  // Add a Key/Value pair to the Dictionary
+            //errorMessages.Add("Test", "A new message!");  // If you attempt to add the same Key again, the 
+                                                            // Dictionary will throw an Exception
+            errorMessages["Test"] = "A new message";    // You may index the Dictionary by the Key to access the Value.
+                                                        // The Value may be changed in this way, but the Key is not editable.
+
+            errorMessages["AnotherTest"] = "SomeMessage";  // If you attempt to assign to a Key that does not already
+                                                           // exist, the Key will be created and then the supplied Value 
+                                                           // assigned to the Key
+
+            Console.WriteLine(errorMessages["AnotherTest"]);  // You may access the Value for extraction using the indexer as well
+        }
+
+    }
+}
---
+++ MyFirstBlazorSolution/MyFirstBlazorApp/Data/SelectionList.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstBlazorApp.Data  // Note that the namespace assigned is the ProjectName/FolderName
+                                 // A using or @using directive must be used wherever the class is being used
+{
+    // Class used in the Lists and Slider page
+    public class SelectionList
+    {
+        public int ValueID { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
