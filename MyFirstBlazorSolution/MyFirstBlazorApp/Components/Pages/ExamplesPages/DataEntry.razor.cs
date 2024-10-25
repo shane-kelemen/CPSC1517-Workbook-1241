62f04f6eb7462c1914a6be54fd28cd53787dc27f
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2024-10-24 21:51:39 -0600

    October 24 - Added functionality for EmploymentReport and Privacy page - Fully Commented

--- MyFirstBlazorSolution/MyFirstBlazorApp/Components/Pages/ExamplesPages/DataEntry.razor.cs
+++ MyFirstBlazorSolution/MyFirstBlazorApp/Components/Pages/ExamplesPages/DataEntry.razor.cs
@@ -13,18 +13,36 @@ namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
         // specifying the "Key".  Both the Key and the Value may be any data type.
         private Dictionary<string, string> errorMessages = new Dictionary<string, string>();
 
+        // The following fields have been included to back up the controls on the Data Entry page 
         private string employmentTitle = "";
         private DateTime startDate;
         private double employedYears = 0;
         private SupervisoryLevel employmentLevel;
-        private Employment employment;
+        //private Employment employment;            // Used to store an Employmnet class instance.  In this particular page
+                                                    // it is actually not needed at the class level because it is being used 
+                                                    // in only one place, just before saving the instance to the file in the 
+                                                    // CollectEmploymentInfo method.  Thus it could have been created in that 
+                                                    // method and then allowed to go out of scope after the instance was no 
+                                                    // longer needed.
 
+        // Inject blocks are needed when we wish to use services from other Assemblies in our code behind files
+        // Make sure there is an appropriate using block placed at the top of the page if an error is indicated. 
+        // Often no error will occur and/or the using statement will be included automatically if required.
+
+        // IJSRuntime is in Microsoft.JSInterop
+        // Used to employ some javascript functionality into the page.  In this case a popup that we wish to use to 
+        // extract a confirmation from the user.
         [Inject]
         private IJSRuntime JSRuntime { get; set; }
 
+        // NavigationManager is in Microsoft.AspNetCore.Components 
+        // Used to move from one page to another through C# code rather than an HTML anchor
         [Inject]
         private NavigationManager NavigationManager { get; set; }
 
+        // IWebHostEnvironment is in Microsoft.AspNetCore.Hosting
+        // Used to extract information about the current execution environment.  We are using it 
+        // to extract the path of our current working directory.
         [Inject]
         private IWebHostEnvironment WebHostEnvironment { get; set; }
 
@@ -45,14 +63,19 @@ namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
             //Console.WriteLine(errorMessages["AnotherTest"]);  // You may access the Value for extraction using the indexer as well
 
             startDate = DateTime.Today;
+
         }
 
+
+        // Method for gathering the information from the page controls, validating the data, and storing it to the file 
+        // if all data is valid.
         private void CollectEmploymentInfo()
         {
+            // Standard clearing of user messaging fields
             feedback = "";
             errorMessages.Clear();
 
-            #region Business Rules
+            #region Business Rules - Similar to the data validation we do in class properties
             // Title must be present, must have at least one character
             // Start date must be today or in the past
             // Years of employment may not be less than zero
@@ -74,25 +97,34 @@ namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
 
             if (errorMessages.Count > 0)
             {
-                return;
+                return;  // An example of a valid early return.  We have detected illegal data.  No reason to continue.
             }
             #endregion
 
 
+            // If we get to this point, we have all legal data.  Show this in the feedback
             feedback = $"Entered data is {employmentTitle},{startDate},{employedYears},{employmentLevel}";
 
-            string appRootPath = WebHostEnvironment.ContentRootPath;
-            string csvFilename = $@"{appRootPath}/Data/Employments.csv";
+            // We need to create and/or append to a file
+            string appRootPath = WebHostEnvironment.ContentRootPath;  // Find out where the working directory is
+            string csvFilename = $@"{appRootPath}/Data/Employments.csv"; // Add the rest of the pathing to where we want the 
+                                                                         // file or where it already exists
 
             try
             {
-                employment = new Employment(employmentTitle, employmentLevel, startDate, employedYears);
+                // Because this instance is transient (temporarily needed), it is better to declare it in the method
+                // and then let it be destroyed (go out of scope) when we are finished with it.
+                Employment employment = new Employment(employmentTitle, employmentLevel, startDate, employedYears);
 
-                string line = $"{employment.ToString()}\n";
+                string line = $"{employment.ToString()}\n";  // This line arguably is not needed.  Could combine with the next line.
+                                                             // The ToString() may also be eliminate because it will be called automagically
+                                                             // by the runtime.
                 File.AppendAllText(csvFilename, line);
 
-
+                //File.AppendAllText(csvFilename, $"{employment}\n");  // A one-line version of the previous two lines of code
             }
+            // The following catch blocks have been included so we may display different message dependent on the type 
+            // of Exception encountered
             catch (FormatException ex)
             {
                 errorMessages.Add($"Format Error: {errorMessages.Count + 1}", GetInnerException(ex).Message );
@@ -109,13 +141,18 @@ namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
             {
                 errorMessages.Add($"Argument Error: {errorMessages.Count + 1}", GetInnerException(ex).Message);
             }
-            catch (Exception ex)
+            catch (Exception ex)  // Always have one block that can catch all Exceptions
             {
                 errorMessages.Add($"General Error: {errorMessages.Count + 1}", GetInnerException(ex).Message);
             }
 
         }
 
+        /// <summary>
+        /// Method for getting to the root cause of an Exception
+        /// </summary>
+        /// <param name="ex">Provide the Highest Level exception to be mined for the root cause</param>
+        /// <returns></returns>
         private Exception GetInnerException(Exception ex)
         {
             while(ex.InnerException != null)
@@ -126,17 +163,28 @@ namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
             return ex;
         }
 
-        private async Task Clear()
-        {
-            feedback = "";
+        // The following require the IJSRuntime [Inject] that you find above
+        /// <summary>
+        /// This method will reset all of the web controls to their default values after first seeking 
+        /// confirmation from the user that they wish to perform the action
+        /// </summary>
+        /// <returns></returns>
+        private async Task Clear()  // The Task is marked async because we must wait for a user response and we
+        {                           // do not know how long that will take
+           
+            feedback = "";  // Reset the feedback messaging only
 
+            // Initialize the array that will display the confirmation message to the user
             object[] messageline = new object[]
                 {"Clearing will lose all unsaved data."
                     + " Are you sure you want to clear the form?" };
 
+            // The await holds the processing of them page until the user either confirms or cancels the action.
             if(await JSRuntime.InvokeAsync<bool>("confirm", messageline))
             {
-                errorMessages.Clear();
+                errorMessages.Clear();  // Only clear the error messages on user confirmation
+                
+                // Reset all other controls to their starting values on confirmation
                 employmentTitle = "";
                 startDate = DateTime.Today;
                 employedYears = 0;
@@ -144,6 +192,8 @@ namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
             }
         }
 
+        // Go to the reporting page using the C# NavigationManager rather than an HTML anchor
+        // This require the matching [Inject] that you will find near the top of the page
         private void GoToReport()
         {
             NavigationManager.NavigateTo("report");
---
+++ MyFirstBlazorSolution/MyFirstBlazorApp/Components/Pages/ExamplesPages/EmploymentReport.razor.cs
@@ -0,0 +1,101 @@
+﻿using MyWebClassLibrary;
+
+namespace MyFirstBlazorApp.Components.Pages.ExamplesPages
+{
+    public partial class EmploymentReport
+    {
+        // Standard fields for the feedback and error messages sections on the razor page
+        private string feedback = "";
+        private List<string> errorMessages = new List<string>();
+
+        // The list that will nbe examine to determin what to display on the front page.  Will
+        // hold all of the valid Employment instances we are able to Parse from the file.
+        private List<Employment> employments = null;
+
+
+        private Exception GetInnerException(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            return ex;
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            ReadEmploymentsFromFile();  // Call the file Parsing method on page load.
+        }
+
+
+        void ReadEmploymentsFromFile()
+        {
+            // Reset the user messaging fields
+            feedback = "";
+            errorMessages.Clear();
+
+            // On this page we will specify the working directory (current content directory) differently than the DataEntry page
+            // This type of pathing will start at the top of your web application (meaning the root directory).
+            string filePath = @"./Data";
+
+            // Files for testing different things.  Note that these are all in the Data folder
+            // 0 - The good file.  All data is valid and so will display without errors
+            // 1 - An Empty file to have our empty List message display
+            // 2 - A file with an incorrect file extension.  Should cause the invalid file message to display
+            // 3 - A non-existant file.  Should cause the invalid file message to display
+            // 4 - A file with a row that is invalid.  The valid rows will Parse and display correctly.
+            //     Some errors for the bad lines will also be displayed.  If all bad line, then the empty list message
+            //     would display with the errors instead of a table.
+            string[] filenames = { "Employments.csv", "Empty.csv", "TextFile.txt", "", "BadData.csv" };
+            string filename = $@"{filePath}/{filenames[4]}";  // Adjust the index to one of the above to see the different results.
+
+
+            string[] employmentsData = null;   // Array for holding the results of the ReadAllLines() operation if successfully read
+            Employment employment = null;      // Object to hold each created employment instance when Parsed
+            int recordIndex = 1;               // For tracking which line of the file we are reading.  Wanted to write more complete
+                                               // error message when a bad Parse is encountered.
+
+            try
+            {
+                // Attempt to run the Parse if the file exists and it has the correct file extension (.csv)
+                if (File.Exists(filename) && Path.GetExtension(filename) == ".csv")
+                {
+                    employments = new List<Employment>();  // Create the List so that the bad file message does not display
+                                                           // Still allows the No Employments message to display
+
+                    employmentsData = File.ReadAllLines(filename);  // Read all of the data from the file
+
+                    // Attempt to Parse each line in the file to a valid Employment instance
+                    foreach (string empFromFile in employmentsData)
+                    {
+                        try
+                        {
+                            employment = Employment.Parse(empFromFile);  // If this line throws an exception
+                            employments.Add(employment);
+                        }
+                        catch (Exception ex)
+                        {
+                            // Add an error message to the user indicating the row that had a problem.
+                            errorMessages.Add($"employmentsData {recordIndex} : {ex.Message}");
+                        }
+
+                        ++recordIndex;  // Always increase the recordIndex
+                    }
+                }
+                else
+                {
+                    throw new Exception($"File {filename} does not exist or wrong extension!");
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessages.Add(GetInnerException(ex).Message);  // For displaying error messages we cannot predict.
+                                                                   // Usually file operation mishaps.
+            }
+        }
+
+    }
+}
