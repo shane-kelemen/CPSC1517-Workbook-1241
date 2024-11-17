fa0abcbb6318aab225e61211366af8c5306e0c0c
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2024-11-16 20:17:49 -0700

    Update WestWindContext.cs - November 14 commenting completed

--- DBInteractionSolution/DBInteractionSystem/DAL/WestWindContext.cs
+++ DBInteractionSolution/DBInteractionSystem/DAL/WestWindContext.cs
@@ -53,7 +53,7 @@ internal partial class WestWindContext : DbContext
     public virtual DbSet<Territory> Territories { get; set; }
 
 //    IF YOU INCLUDED THE CONNECTION STRING WHEN COMPLETING THE REVERSE ENGINEERING, YOU MUST COMMENT THIS FOLLOWING CODE OUT AFTER 
-//    YOU INCLUDE A CONNECTION STRING INTO YOUR appsettings.json file.
+//    YOU INCLUDE A CONNECTION STRING INTO YOUR appsettings.json FILE.
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
 //        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=WestWind;Integrated Security=True;encrypt=false");
