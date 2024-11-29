0fcc602c54f907f6f45b947fbfee9052e0853b16
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2024-11-28 18:53:59 -0700

    November 26 - Commenting Added to ProductServices class and ProductCRUD.razor.cs

--- DBInteractionSolution/DBInteractionSystem/BLL/ProductServices.cs
+++ DBInteractionSolution/DBInteractionSystem/BLL/ProductServices.cs
@@ -21,6 +21,18 @@ namespace DBInteractionSystem.BLL
         }
         #endregion
 
+        /// <summary>
+        /// This method will attempt to add a new product into the Products table of the Westwind database,
+        /// or more accurately, the database that is being accessed through the DbContext (WestWindContext) 
+        /// provided to the constructor of the ProductServices class above from the associated Transient in
+        /// the WestWindExtensions class.
+        /// </summary>
+        /// <param name="inputProduct">An instance of the Product entity.  Definition found in the Entities 
+        ///                             folder of the system library.</param>
+        /// <returns>The ProductID automatically assigned to the product by the database.  Identity column.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if a Product instance is not provided.</exception>
+        /// <exception cref="AggregateException">Throws a collection of Exceptions encountered during data validation
+        ///                                         and business rules enforcement.</exception>
         public int Product_Add(Product inputProduct)
         {
             #region Data Validation
@@ -33,6 +45,9 @@ namespace DBInteractionSystem.BLL
                 throw new ArgumentNullException("You must supply product information to be saved!");
             }
 
+            // All data should be checked to ensure it is legal.  An example check for the required ProductName
+            // is shown here.  Other data checks may include valid ranges for values such as >= 0 for UnitPrice.
+            // Appropriate messages should be added to the Exception collection as shown in the ProductName check.
             if (string.IsNullOrWhiteSpace(inputProduct.ProductName))
             {
                 errors.Add(new Exception("The product name must be provided!"));
@@ -45,14 +60,18 @@ namespace DBInteractionSystem.BLL
             // case, we will allow a supplier to give us only one product with the same name and description.
             // For example:  Supplier 25, may provide Milk in a QuantityPerUnit of 4L, but they may not
             //                have a second product with the same two features.  However, Supplier 30 may 
-            //                also supply Milk in a quantity of 4L.
+            //                also supply Milk in a quantity of 4L, or Supplier 25 may also provide Milk in a
+            //                QuantityPerUnit of 2L.
             bool exists = false;
 
+            // Check to see if the product with the provided features already exists 
             exists = _westWindContext.Products
                                     .Any(product => product.SupplierID == inputProduct.SupplierID
                                                     && product.ProductName == inputProduct.ProductName
                                                     && product.QuantityPerUnit == inputProduct.QuantityPerUnit);
 
+            // If the product with the provided features does exist, an an Exception with an appropriate and descriptive
+            // message to the Exception collection.
             if (exists)
             {
                 errors.Add(new Exception($"Product {inputProduct.ProductName} "
@@ -66,6 +85,12 @@ namespace DBInteractionSystem.BLL
             // of the problems and exit without performing any database manipulation.
             if(errors.Count > 0)
             {
+                // This Clear() of the ChangeTracker is not required unless a database data change has occurred,
+                // but it does not hurt anything to be double sure.
+                _westWindContext.ChangeTracker.Clear();
+
+                // This throws the Exception collection back to the calling scope so that the contained errors
+                // may be used.
                 throw new AggregateException("Server operation detected errors: ", errors);
             }
 
@@ -73,16 +98,20 @@ namespace DBInteractionSystem.BLL
             // then we may proceed with database operations.
             
             // Accessing a database may always possibly result in unexpected errors.  For instance, a
-            // communication breakdown with the database engine.
+            // communication breakdown with the database engine, so it is advisable to use a try/catch
+            // to perform the operation.  Add any errors to the Exception collection.
             try
             {
-                // This is where the insert operation is being attempted.
+                // This is where the insert operation is being attempted, but the operation is only being tested in memory,
+                // not saved to the database in SQL Server yet.  This is similar to how a transaction works in SQL.  Above,
+                // when we Clear() the ChangeTracker, we are essentially performing a rollback on the transaction.  Later, we
+                // will attempt to perform a commit when we use the SaveChanges() method of the context.
                 _westWindContext.Products.Add(inputProduct);
             }
             catch (Exception ex)
             {
                 errors.Add(ex);  // Add any error that may be caught into our collection of errors to be
-                                 // thrown back to the called if a database interaction problem occurs
+                                 // thrown back to the caller if a database interaction problem occurs
             }
 
             // If any errors whatsoever are encountered, we must clear any changes that may have occurred
@@ -90,18 +119,35 @@ namespace DBInteractionSystem.BLL
             if(errors.Count > 0)
             {
                 // The ChangeTracker stores all changes to be made to the database when a SaveChanges() call
-                // is used.  This would include any changes left over in the CHangeTracker during a failed 
+                // is used.  This would include any changes left over in the ChangeTracker during a failed 
                 // database operation.  Thus, we must Clear() the ChangeTracker on error detection.
                 _westWindContext.ChangeTracker.Clear();
 
                 throw new AggregateException("Server operation detected errors: ", errors);
             }
-            // If no errors are encountered, then we may save all changes to the database.
+
+            // If no errors are encountered, then we may attempt to save all changes to the database.
             else
             {
-                _westWindContext.SaveChanges();
+                // Once again, a try/catch should be used here, as the action of pushing the data changes to the
+                // database may encounter problems that cannot be or have not been predicted, such as a communications
+                // error, or the truncation of data.
+                try
+                {
+                    _westWindContext.SaveChanges();
+                }
+                catch(Exception ex)
+                {
+                    // Once again, be sure to Clear() the database changes before throwing the received Exception back
+                    // to the user.  The error collection is not required here as there can only be one Exception thrown
+                    // if you have gotten to this point.
+                    _westWindContext.ChangeTracker.Clear();
+                    throw ex;
+                }
             }
 
+            // If the SaveChanges() method is successful, a ProductID will have been assigned to the product that we tried
+            // to save.  We have decided to send it back to the user.
             return inputProduct.ProductID;
         }
     }
--- DBInteractionSolution/DBInteractionWebApp/Components/Examples/ProductCRUD.razor.cs
+++ DBInteractionSolution/DBInteractionWebApp/Components/Examples/ProductCRUD.razor.cs
@@ -21,29 +21,44 @@ namespace DBInteractionWebApp.Components.Examples
 
 
         [Inject]
-        private ProductServices ProductServices { get; set; }
+        private ProductServices ProductServices { get; set; }      // Grants access to the methods in the ProductServices class of
+                                                                   // the system library.  Remember the [Inject].
 
         [Inject]
-        private CategoryServices CategoryServices { get; set; }
+        private CategoryServices CategoryServices { get; set; }    // Grants access to the methods in the CategoryServices class of
+                                                                   // the system library.  Remember the [Inject].
 
         [Inject]
-        private SupplierServices SupplierServices { get; set; }
+        private SupplierServices SupplierServices { get; set; }    // Grants access to the methods in the SupplierServices class of
+                                                                   // the system library.  Remember the [Inject].
 
 
+        /// <summary>
+        /// Time to Initialize the Blazor page controls
+        /// </summary>
         protected override void OnInitialized()
         {
             base.OnInitialized();
 
-            editContext = new EditContext(CurrentProduct);
-            validationMessageStore = new ValidationMessageStore(editContext);
+            // The following are required to make validation of data on the razor page possible
+            editContext = new EditContext(CurrentProduct);  // Create a context that will be applied to the CurrentProduct
+            validationMessageStore = new ValidationMessageStore(editContext);  // Create a ValidationStore for the EditContext just created
 
+            // Retrieve a List of all the Categories and Suppliers to poulate the select controls
             categories = CategoryServices.Categories_GetAll();
             suppliers = SupplierServices.Suppliers_GetAll();
         }
 
 
+        /// <summary>
+        /// This method will collect the information from the razor page and send it to the ProductService
+        /// class so that a database addition may be attempted.
+        /// </summary>
         private void OnCreate()
         {
+            // Reset all of the fields for feedback, error messaging, previous displayed validator errors,
+            // and the ProductID in case a previous submit was already successful which would cause the 
+            // ProductID to be something other than zero.
             feedback = "";
             errorMessages.Clear();
             validationMessageStore.Clear();
@@ -51,10 +66,16 @@ namespace DBInteractionWebApp.Components.Examples
 
             try
             {
+                // When a submit is attempted
                 if (editContext.Validate())
                 {
+                    // Check for illegal or missing data in any of the fields that you wish to check on the
+                    // client side
                     if (CurrentProduct.CategoryID == 0)
                     {
+                        // For any item where the data is unacceptable, add an error message to the message store
+                        // using the following pattern.  Notice it is consistent for all three checks included here
+                        // aside from the item being accessed in the CurrentProduct and the message text.
                         validationMessageStore.Add(editContext.Field(nameof(CurrentProduct.CategoryID)), 
                                                         "You must select a category");
                     }
@@ -71,10 +92,14 @@ namespace DBInteractionWebApp.Components.Examples
                                                         "The unit price must be greater than 0");
                     }
 
+                    // If there are any messages for the editContext
                     if(editContext.GetValidationMessages().Any())
                     {
+                        // Have those messages displayed
                         editContext.NotifyValidationStateChanged();
                     }
+                    // If there are no messages, proceed with a call to the ProductServices method for adding
+                    // a new Product to the database.  Display success feedback if no Exceptions are thrown.
                     else
                     {
                         int newProductID = ProductServices.Product_Add(CurrentProduct);
@@ -83,18 +108,18 @@ namespace DBInteractionWebApp.Components.Examples
                     }                 
                 }
             }
-            catch (ArgumentNullException ex)
+            catch (ArgumentNullException ex)  // Triggered if no Product instance is provided to the service method
             {
                 errorMessages.Add(ex.Message);
             }
-            catch (AggregateException ex)
+            catch (AggregateException ex)   // Triggered if data validation, business rules, or temporary data modifications encountered
             {
                 foreach(Exception inner in ex.InnerExceptions)
                 { 
                     errorMessages.Add(inner.Message);
                 }
             }
-            catch (Exception ex)
+            catch (Exception ex)    // Triggered if the final SaveChanges() operation throws an unexpected exception.
             {
                 errorMessages.Add(HelperClass.GetInnerException(ex).Message);
             }
